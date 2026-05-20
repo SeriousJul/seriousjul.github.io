2659e7bb9e110ac4698c18a228c87dcfd988312d
Julien Antony <jul.antony@gmail.com>
2026-05-20 22:22:12 +0200

    chore: change deployment branch to main

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SeriousJul', // Usually your GitHub org/user name.
   projectName: 'seriousjul.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages', // The branch to deploy to.
+  deploymentBranch: 'main', // The branch to deploy to.
 
   onBrokenLinks: 'throw',
 
